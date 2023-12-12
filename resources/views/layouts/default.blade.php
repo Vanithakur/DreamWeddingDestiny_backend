4fd49994f9b744348700e81e0da2497c0bbc7ce7
umesh <umesh.patidar@mangoitsolutions.in>
2023-12-12 14:58:38 +0530

    added the layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -27,22 +27,27 @@
 </head>
 
 <body>
-    @yield('header')
-    @yield('content')
-    @stack('scripts')
-    <!-- jQery -->
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <!-- bootstrap js -->
-    <script src="js/bootstrap.js"></script>
-    <!-- slick slider -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick.min.js"></script>
-    <!-- custom js -->
-    <script src="js/custom.js"></script>
-    <!-- Google Map -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
-    </script>
-    <!-- End Google Map -->
-    @yield('footer')
+    <div class="main_body_content">
+        <div class="hero_area">
+            @yield('header')
+            @yield('content')
+            @stack('scripts')
+            <!-- jQery -->
+            <script src="js/jquery-3.4.1.min.js"></script>
+            <!-- bootstrap js -->
+            <script src="js/bootstrap.js"></script>
+            <!-- slick slider -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick.min.js"></script>
+            <!-- custom js -->
+            <script src="js/custom.js"></script>
+            <!-- Google Map -->
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+            </script>
+            <!-- End Google Map -->
+            @yield('footer')
+        </div>
+    </div>
+
 </body>
 
 </html>
