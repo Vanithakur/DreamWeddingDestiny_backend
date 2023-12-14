9e6e207c6d0434783be979d65e30f1d17e19eae2
shivani thakur <st5930123@gmail.com>
2023-12-14 14:38:51 +0530

    added the super admin dashboard

--- app/Http/Controllers/admin/ServiceController.php
+++ app/Http/Controllers/admin/ServiceController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = Service::all();
+        // Pass the services data to the view
+        return view('admin.service.index', compact('services'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\ServiceController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -27,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/index', [ProfileController::class, 'index'])->name('service.index');
+    Route::get('/index', [ServiceController::class, 'index'])->name('service.index');
 });
 
 Route::get('/home', [HomeController::class,'home'])->name('home');
